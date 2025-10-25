edefbef15b7755736e88b5ebc65f93c05dad1927
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-10-25 22:40:47 +0300

    saving to file

--- Student_Management_System/src/StudentDatabase.java
+++ Student_Management_System/src/StudentDatabase.java
@@ -1,37 +1,38 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDatabase {
+    private final String filename = "data.txt";
     private List<Student> students;
 
     public StudentDatabase(List<Student> students) {
         this.students = students;
     }
 
-    
     public boolean addStudent(Student student) {
-       
+
         for (Student s : students) {
             if (s.getStudentId() == student.getStudentId()) {
-                return false; 
+                return false;
             }
         }
         students.add(student);
         saveToFile();
         return true;
     }
-    
-        public boolean deleteStudent(int studentId) {
+
+    public boolean deleteStudent(int studentId) {
         boolean removed = students.removeIf(s -> s.getStudentId() == studentId);
-        
+
         if (removed) {
             saveToFile();
         }
-        
+
         return removed;
     }
 
-   
     public Student searchStudentById(int studentId) {
         for (Student s : students) {
             if (s.getStudentId() == studentId) {
@@ -41,7 +42,6 @@ public class StudentDatabase {
         return null;
     }
 
-    
     public List<Student> searchStudentByName(String fname) {
         List<Student> result = new ArrayList<>();
         for (Student s : students) {
@@ -67,7 +67,7 @@ public class StudentDatabase {
             saveToFile();
         } else {
             List<Student> result = searchStudentByName(updatedStudent.getFname());
-            if(result.size() == 1) {
+            if (result.size() == 1) {
                 unupdatedStudent = result.get(0);
                 unupdatedStudent.setStudentId(updatedStudent.getStudentId());
                 unupdatedStudent.setDepartment(updatedStudent.getDepartment());
@@ -75,9 +75,10 @@ public class StudentDatabase {
                 unupdatedStudent.setGPA(updatedStudent.getGPA());
                 unupdatedStudent.setGender(updatedStudent.getGender());
                 saveToFile();
-            } else{
-                if(result.size() > 1) {
-                    throw new IllegalArgumentException("Multiple students found with the same name; use Student ID to update.");
+            } else {
+                if (result.size() > 1) {
+                    throw new IllegalArgumentException(
+                            "Multiple students found with the same name; use Student ID to update.");
                 } else {
                     throw new IllegalArgumentException("Student not found.");
                 }
@@ -86,12 +87,28 @@ public class StudentDatabase {
 
     }
 
-    public void saveToFile() {
-       // to be implemented
+    public void saveToFile() throws IOException {
+        try{
+        FileWriter fw = new FileWriter(this.filename, false);
+        for (int i = 0; i < students.size(); i++) {
+            fw.write(lineRepresentation(students.get(i)) + "\n");
+        }
+
+        fw.close();
+        }
+        catch(IOException e){
+            System.out.println("An error occurred while trying to save data: " + e.getMessage());
+            return;
+        }
     }
 
     public void loadFromFile() {
         // to be implemented
-        
+
+    }
+
+    private String lineRepresentation(Student student) {
+        return student.getStudentId() + "," + student.getFname() + "," + student.getGender() + "," +
+                student.getDepartment() + "," + student.getGPA() + "," + student.getAge();
     }
 }
