4c4729bd5fae975aaf5eadfbf3e232b86180d669
Abdullah Amr <innabdullah.amr@gmail.com>
2025-10-26 12:55:01 +0300

    basic windows

---
+++ Student_Management_System/src/MainWindow.java
@@ -0,0 +1,83 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow extends JFrame {
+
+    private JPanel mainPanel;
+    private JButton addStudentBtn;
+    private JButton viewStudentsBtn;
+    private JButton deleteStudentBtn;
+    private JButton updateStudentBtn;
+    private JButton searchStudentBtn;
+    private JButton logoutBtn;
+    private JTextField clickOnTheNeededTextField;
+    private JLabel Welcome;
+
+    public MainWindow() {
+
+        ImageIcon image = new ImageIcon("admin-icon.jpg");
+        setIconImage(image.getImage());
+        setTitle("Student Management System");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setContentPane(mainPanel);
+        pack();
+        setVisible(true);
+
+
+        addStudentBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new AddStudentWindow();
+                dispose();
+            }
+        });
+
+
+        viewStudentsBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ViewStudentsWindow();
+                dispose();
+            }
+        });
+
+
+        deleteStudentBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new DeleteStudentWindow();
+                dispose();
+            }
+        });
+
+
+        updateStudentBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new UpdateStudentWindow();
+                dispose();
+            }
+        });
+
+
+        searchStudentBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new SearchStudentWindow();
+                dispose();
+            }
+        });
+
+
+        logoutBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Login();
+                dispose();
+            }
+        });
+    }
+}
