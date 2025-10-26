bc267e65e782e88afc66dfa1fa629133f6259200
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-10-26 20:44:50 +0300

    view and main resolved

--- Student_Management_System/src/App.java
+++ Student_Management_System/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
+//        StudentDatabase db = new StudentDatabase();
+//        db.addStudent(new Student(1, "Alice", 20, "male", "Computer Science", 3.5f));
+//        db.addStudent(new Student(7, "Bob", 22, "male", "Software Engineering", 3.7f));
+//
+//        db.saveToFile();
+
         Login start =  new Login();
     }
 }
