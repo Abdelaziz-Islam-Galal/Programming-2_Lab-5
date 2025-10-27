b28a658a3e5643aedc4104e4b32b57f9baaca94a
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-10-27 18:48:51 +0300

    deployed add student

--- Student_Management_System/src/MainWindow.java
+++ Student_Management_System/src/MainWindow.java
@@ -65,6 +65,7 @@ public class MainWindow extends JFrame {
         DepartBox.addItem("Civil Engineering");
         DepartBox.addItem("Biomedical Engineering");
     }
+
     private void addStudent() {
         try {
             String name = nameField.getText();
@@ -92,22 +93,25 @@ public class MainWindow extends JFrame {
             }
             Student s1 = new Student(id, name, age, gender, department, gpa);
             StudentDatabase sdb = new StudentDatabase();
+
             if (!sdb.addStudent(s1)) {
                 JOptionPane.showMessageDialog(this, "Student not added (Student id already exists)",
                         "Input Error", JOptionPane.ERROR_MESSAGE);
                 return;
             }
+
             JOptionPane.showMessageDialog(this, "Student added successfully",
                     "Success", JOptionPane.INFORMATION_MESSAGE);
-            // Clear fields after successful addition
+
             clearFields();
-            // Refresh the table
+
             refreshTable();
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(this, "Please enter valid numbers for Age, ID, and GPA",
                     "Input Error", JOptionPane.ERROR_MESSAGE);
         }
     }
+
     private void clearFields() {
         nameField.setText("");
         ageField.setText("");
@@ -116,9 +120,10 @@ public class MainWindow extends JFrame {
         GenderBox.setSelectedIndex(0);
         DepartBox.setSelectedIndex(0);
     }
+
     private void refreshTable() {
         DefaultTableModel model = (DefaultTableModel) students.getModel();
-        model.setRowCount(0); // Clear existing rows
+        model.setRowCount(0);
         StudentDatabase data = new StudentDatabase();
         List<Student> studentList = data.getAllStudents();
         for (Student s : studentList) {
